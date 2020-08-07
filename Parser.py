d2c44b794d1292173b0e2342632e3f9ba3de489f
Anastasia <Nastya.Shakaryan@yandex.ru>
2020-08-07 21:38:15 +0400

    Threads

--- Parser.py
+++ Parser.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
+import threading
+
 class Parser():
     URL = 'https://store.playstation.com/ru-ru/grid/STORE-MSF75508-GAMEGENREFIGHTIN/1'
     HEADERS = {'user-agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Mobile Safari/537.36', 'accept': '*/*'}
@@ -12,6 +14,7 @@ class Parser():
             return (self.get_content(html.text))
         else:
             print ('Error')
+
     def get_html (self, url, params = None):
         r = requests.get(url, headers = self.HEADERS, params = params)
         return r
@@ -19,18 +22,14 @@ class Parser():
     def get_content(self, html):
         links = []
         soup = BeautifulSoup(html, 'html.parser')
-        items = soup.find_all('a', class_='grid-cell__link internal-app-link ember-view')
+        items = soup.find_all('a', {'class':'grid-cell__link internal-app-link ember-view'})
         for item in items:
-            link = item['href']
-
-            link = self.HOST + link
+            link =self.HOST + item['href']
+      #      l = self.get_gb(link)
             links.append(link)
         return links
-    def get_gb(self):
-        links = self.parse()
-        print(links)
-        list = []
-        for link in links:
+
+    def get_gb(self, link):
             html = self.get_html(link)
             ht = html.text
             soup = BeautifulSoup(ht, 'html.parser')
@@ -38,11 +37,21 @@ class Parser():
                 item = soup.find('span',{'class':'info-header__detail-item info-header__detail-item--file-size'}).text
             except:
                 item = None
-            list.append(item)
-        print(list)
+            list_gb.append(item)
+   #         return item
 
 parse = Parser()
 start_time = datetime.now()
-parse.get_gb()
+links = parse.parse()
+print(links)
+list_gb = []
+threads = []
+for link in links:
+    thread = threading.Thread(target=parse.get_gb, args=[link])
+    threads.append(thread)
+    thread.start()
+for thread in threads:
+    thread.join()
+print(list_gb)
 finish_time = datetime.now()
 print(f'Затраченное время {finish_time-start_time}')
